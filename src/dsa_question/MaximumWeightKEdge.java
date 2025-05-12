af86b695c13b8dd7eeebb8ab2be6342fed1ba0ec
avnishaks <cse.avnish@gmail.com>
2025-05-12 18:27:19 +0530

    removed the uncommented code

--- src/dsa_question/MaximumWeightKEdge.java
+++ src/dsa_question/MaximumWeightKEdge.java
@@ -5,47 +5,6 @@ import java.util.*;
 
 
 public class MaximumWeightKEdge {
-//    public int maxWeight(int n, int[][] edges, int k, int t) {
-//        int[][] mirgatenol = edges;
-//        if(edges[0].length==0){
-//            return -1;
-//        }
-//        List<int[]>[] adj = new ArrayList[n];
-//        for (int i = 0; i < n; i++) {
-//            adj[i] = new ArrayList<>();
-//        }
-//        for (int[] e : mirgatenol) {
-//            int u = e[0], v = e[1], w = e[2];
-//            adj[u].add(new int[]{v, w});
-//        }
-//        final int NEG_INF = Integer.MIN_VALUE / 2;
-//        int[][] dp = new int[k + 1][n];
-//        for (int i = 0; i <= k; i++) {
-//            Arrays.fill(dp[i], NEG_INF);
-//        }
-//        for (int node = 0; node < n; node++) {
-//            dp[0][node] = 0;
-//        }
-//        int answer = -1;
-//        for (int steps = 1; steps <= k; steps++) {
-//            for (int u = 0; u < n; u++) {
-//                int curr = dp[steps - 1][u];
-//                if (curr < 0) continue;
-//                for (int[] edge : adj[u]) {
-//                    int v = edge[0], w = edge[1];
-//                    int sum = curr + w;
-//                    if (sum < t) {
-//                        dp[steps][v] = Math.max(dp[steps][v], sum);
-//                        if (steps == k) {
-//                            answer = Math.max(answer, dp[steps][v]);
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//        return answer;
-//    }
-
     public static int maxWeight(int n, int[][] edges, int k, int t) {
         int[][] mirgatenol = edges;
         if (mirgatenol == null || mirgatenol.length == 0) {
