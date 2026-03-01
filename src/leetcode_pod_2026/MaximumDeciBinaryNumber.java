881663a6466359451b4d162d0f056a2a6a07e57e
avnishaks <cse.avnish@gmail.com>
2026-03-02 03:18:35 +0530

    added leetcode pod 2nd march

---
+++ src/leetcode_pod_2026/MaximumDeciBinaryNumber.java
@@ -0,0 +1,17 @@
+package leetcode_pod_2026;
+
+public class MaximumDeciBinaryNumber {
+    public int minPartitions(String n) {
+        int ans=0;
+        for(int i=0;i<n.length();i++){
+            ans=Math.max(ans,n.charAt(i)-'0');
+        }
+        return ans;
+
+    }
+    public static void main(String[] args) {
+        String s="32";
+        int ans= new MaximumDeciBinaryNumber().minPartitions(s);
+        System.out.println(ans);
+    }
+}
